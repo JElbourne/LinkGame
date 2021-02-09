53a339559611506649c5e3da2b24cb226c36d16b
Jay Elbourne <jayelbourne@gmail.com>
2021-02-08 22:44:46 -0500

    Added the game over screen

---
+++ Source/LinkGame/Public/Characters/Enemies/LinkEnemyJumping.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LinkEnemyBase.h"
+#include "LinkEnemyJumping.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LINKGAME_API ALinkEnemyJumping : public ALinkEnemyBase
+{
+	GENERATED_BODY()
+	
+};
